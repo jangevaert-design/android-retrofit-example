fdb2dd47805f606ddfd7b006ca199d11720fddbc
Jan Gevaert <jan.gevaert@gmail.com>
2020-09-22 11:42:25 -0600

    Worked with URL Manipulation: @Path, @Query, @QueryMap and @Url.

--- app/src/main/java/edu/cnm/deepdive/retrofitexample/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/retrofitexample/MainActivity.java
@@ -3,7 +3,9 @@ package edu.cnm.deepdive.retrofitexample;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -13,6 +15,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
   private TextView textViewResult;
+
+  private JsonPlaceHolderApi jsonPlaceHolderApi;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -26,36 +30,79 @@ public class MainActivity extends AppCompatActivity {
         .addConverterFactory(GsonConverterFactory.create())
         .build();
 
-    JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+    jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
 
-    Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
+    //getPosts();
+    getComments();
+  }
+
+  private void getPosts() {
+    Map<String, String> parameters = new HashMap<>();
+    parameters.put("userId", "1");
+    parameters.put("_sort", "1");
+    parameters.put("_order", "desc");
+
+    Call<List<Post>> call = jsonPlaceHolderApi.getPosts(parameters);
 
     call.enqueue(new Callback<List<Post>>() {
       @Override
       public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
-            if (!response.isSuccessful()) {
-              textViewResult.setText("Code: " + response.code());
-              return;
-            }
+        if (!response.isSuccessful()) {
+          textViewResult.setText("Code: " + response.code());
+          return;
+        }
 
-            List<Post> posts = response.body();
+        List<Post> posts = response.body();
 
-            for (Post post : posts) {
-              String content ="";
-              content += "ID: " + post.getId() + "\n";
-              content += "User ID: " + post.getUserId() + "\n";
-              content += "Title: " + post.getTitle() + "\n";
-              content += "Text: " + post.getText() + "\n\n";
+        for (Post post : posts) {
+          String content ="";
+          content += "ID: " + post.getId() + "\n";
+          content += "User ID: " + post.getUserId() + "\n";
+          content += "Title: " + post.getTitle() + "\n";
+          content += "Text: " + post.getText() + "\n\n";
 
-              textViewResult.append(content);
+          textViewResult.append(content);
 
 
-            }
+        }
       }
 
       @Override
       public void onFailure(Call<List<Post>> call, Throwable t) {
-            textViewResult.setText(t.getMessage());
+        textViewResult.setText(t.getMessage());
+      }
+    });
+  }
+
+  private void getComments() {
+    Call<List<Comment>> call = jsonPlaceHolderApi.getComments("https://jsonplaceholder.typicode.com/posts/3/comments");
+
+    call.enqueue(new Callback<List<Comment>>() {
+      @Override
+      public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
+
+        if (!response.isSuccessful()) {
+          textViewResult.setText("Code: " + response.code());
+          return;
+        }
+
+        List<Comment> comments = response.body();
+
+        for (Comment comment : comments) {
+          String content = "";
+          content += "ID: " + comment.getId() + "\n";
+          content += "Post ID: " + comment.getPostId() + "\n";
+          content += "Name: " + comment.getName() + "\n";
+          content += "Email: " + comment.getEmail() + "\n";
+          content += "Text: " + comment.getText() + "\n\n";
+
+          textViewResult.append(content);
+
+        }
+      }
+      @Override
+      public void onFailure(Call<List<Comment>> call, Throwable t) {
+        textViewResult.setText(t.getMessage());
       }
     });
   }
